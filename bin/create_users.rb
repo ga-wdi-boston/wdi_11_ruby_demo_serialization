85520f2a83cf099db5582a1664a42d263a059e99
Tom Dyer <tdyer@ourstage.com>
2014-03-19 03:48:18 -0400

    initial commit

---
+++ bin/create_users.rb
@@ -0,0 +1,26 @@
+require 'faker'
+require_relative '../lib/user'
+
+class CreateUsers
+  def self.create(num_of_users)
+    all_users = []
+
+    num_of_users.times do |i|
+      address = MailingAddress.new(Faker::Address.street_address, Faker::Address.city, Faker::Address.state_abbr, Faker::Address.zip)
+      prefs = UserPreference.new()
+      user = User.new(address, Faker::PhoneNumber.cell_phone, Faker::Internet.email)
+      # puts "Created User #{user}"
+      all_users << user
+    end
+    all_users
+  end
+
+end
+
+
+# file_name = "data/users.yaml"
+# File.open(file_name, "w") do |file|
+#   serialized_users = YAML::dump(all_users)
+#   file.write(serialized_users)
+#   puts "Wrote #{user_count} users to #{file_name}"
+# end
